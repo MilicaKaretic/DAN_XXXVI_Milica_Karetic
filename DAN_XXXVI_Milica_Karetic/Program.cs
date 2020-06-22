05502cb61990f1cb7f21a13959d348332c50c186
MilicaKaretic <milicakaretic1@gmail.com>
2020-06-22 16:44:02 +0200

    log into file and read odd numbers from file implemented

--- DAN_XXXVI_Milica_Karetic/Program.cs
+++ DAN_XXXVI_Milica_Karetic/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -13,6 +14,7 @@ namespace DAN_XXXVI_Milica_Karetic
         public static List<int> list = new List<int>(10000);
         public static int[,] matrix;      
         static Random rnd = new Random();
+        public static string fileName = "FileNumbers.txt";
 
         /// <summary>
         /// Method for generate numbers
@@ -53,9 +55,78 @@ namespace DAN_XXXVI_Milica_Karetic
             }
         }
 
+        public static void ReadFromFile()
+        {
+            lock (fileName)
+            {
+                Monitor.Wait(fileName);
+                using (StreamReader sr = File.OpenText(fileName))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.Write(line + " ");
+                    }
+                }
+            }
+        }
+
+
+        public static void LogIntoFile()
+        {
+            //odd numbers
+            int[] arr = list.Where(i => i % 2 == 1).ToArray();
+
+            lock (fileName)
+            {
+                using (StreamWriter sw = File.CreateText(fileName))
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        sw.WriteLine(arr[i]);
+                    }
+                }
+                Monitor.Pulse(fileName);
+            }
+        }
+
+
+
         static void Main(string[] args)
         {
+            Thread t1 = new Thread(GenerateMatrix)
+            {
+                Name = "GenerateMatrix"
+            };
+            Thread t2 = new Thread(GenerateNumbers)
+            {
+                Name = "GenerateNumbers"
+            };
             
+            //start 1. i 2. threads
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            Thread t3 = new Thread(LogIntoFile)
+            {
+                Name = "OddNumbersToFile"
+            };
+            Thread t4 = new Thread(ReadFromFile)
+            {
+                Name = "ReadNumbers"
+            };
+
+            //start 3. and 4. threads
+            t3.Start();
+            t4.Start();
+            
+            t3.Join();
+            t4.Join();
+
+            Console.ReadKey();
         }
     }
 }
