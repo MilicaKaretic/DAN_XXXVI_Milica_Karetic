6c88a0dddad9caf04d01b4e578e16442f63e5b45
MilicaKaretic <milicakaretic1@gmail.com>
2020-06-22 16:21:30 +0200

    generate matrix and numbers implemented

--- DAN_XXXVI_Milica_Karetic/Program.cs
+++ DAN_XXXVI_Milica_Karetic/Program.cs
@@ -2,20 +2,60 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DAN_XXXVI_Milica_Karetic
 {
     class Program
     {
+        private static object locker = new object();
+        public static List<int> list = new List<int>(10000);
+        public static int[,] matrix;      
+        static Random rnd = new Random();
+
+        /// <summary>
+        /// Method for generate numbers
+        /// </summary>
+        public static void GenerateNumbers()
+        {
+            int num;
+            lock (locker)
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    num = rnd.Next(10, 100);
+                    list.Add(num);
+                }
+                Monitor.Pulse(locker);
+            }
+        }
+
+        /// <summary>
+        /// Method for generate matrix
+        /// </summary>
+        public static void GenerateMatrix()
+        {
+            lock (locker)
+            {
+                matrix = new int[100, 100];
+                while (list.Count < 10000)
+                {
+                    Monitor.Wait(locker);
+                }
+                for (int i = 0; i < 100; i++)
+                {
+                    for (int j = 0; j < 100; j++)
+                    {
+                        matrix[i, j] = list[j + i * 100];
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            
         }
     }
 }
